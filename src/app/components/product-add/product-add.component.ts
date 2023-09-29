b294473a829f4456d0abad2447c273a9aa95555f
Eser <eser.sune@hotmail.com>
2023-09-29 22:06:31 +0300

    last commit for the Camp

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -33,7 +33,9 @@ export class ProductAddComponent implements OnInit {
       let productModel = Object.assign({},this.productAddForm.value)
       this.productService.add(productModel).subscribe(response=>{
         this.toastrService.success(response.message,"Başarılı")
-      },responseError=>{
+      } ,
+      responseError=>{
+        console.log(reportError.name)
         if(responseError.error.Errors.length>0){
           for (let i = 0; i <responseError.error.Errors.length; i++) {
             this.toastrService.error(responseError.error.Errors[i].ErrorMessage
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Category } from '../models/category';
 
